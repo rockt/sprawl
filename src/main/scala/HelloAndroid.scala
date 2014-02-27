cd678fad5c7716dad3ce96d302e7512163c41bdc
rockt <tim.rocktaeschel@gmail.com>
2014-02-27 14:22:23 +0000

    initial android project setup using android-sdk-plugin and sbt

---
+++ src/main/scala/HelloAndroid.scala
@@ -0,0 +1,6 @@
+/**
+ * Created by rockt on 27/02/2014.
+ */
+object HelloAndroid extends App {
+  println("Hello, Android!")
+}
---
+++ src/rockt/sprawl/SprawlActivity.java
@@ -0,0 +1,15 @@
+package rockt.sprawl;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SprawlActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
